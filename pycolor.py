bd60de4e2c0ba6408230026b6d7738817cd06dc3
MD.NAHIDUL ISLAM <151029969+NAHIDUL-79@users.noreply.github.com>
2024-05-10 16:16:09 +0600

    pycolor.py

---
+++ pycolor.py
@@ -0,0 +1,39 @@
+from time import sleep
+BANNER='''
+    """ NAHID COLOR CODES """
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
+'''
+while True:
+    oass =  input('ENTER YOUR PASSWORD : ')
+    if oass =='119887':
+      print(BANNER)
+    else:
+        list_run =['RUN']
+        while True:
+            list_run.append('RUN')
+            print(list_run)
+            sleep(0.2)
+    
